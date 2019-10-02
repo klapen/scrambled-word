9f0248d9447cbb2208fe2697bd488c88dd352ac6
klapen <oasaavedrac@gmail.com>
2019-10-01 19:07:54 -0500

    Implement react-flip-move scrabble example

---
+++ frontend/src/components/App/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Scrabble from '../Scrabble';
+
+function App() {
+  return (
+    <div className="App">
+      <section id="main-content">
+        <Scrabble></Scrabble>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/data/tiles.js
@@ -0,0 +1,12 @@
+const tiles = [
+  { id: 1, letter: 'M', x:1, y: 1 },
+  { id: 2, letter: 'I', x: 2, y: 1 },
+  { id: 3, letter: 'T', x: 3, y: 1 },
+  { id: 4, letter: 'I', x: 4, y: 1 },
+  { id: 5, letter: 'G', x: 5, y: 1 },
+  { id: 6, letter: 'A', x: 6, y: 1 },
+  { id: 7, letter: 'T', x: 7, y: 1 },
+  { id: 8, letter: 'E', x: 8, y: 1 }
+];
+
+export default tiles;
